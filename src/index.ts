c386feaa925e1b7a3c59d9e9a58eba5dff5e6fac
Brian M. Smith <brian@brianmatthewsmith.com>
2023-03-09 08:07:15 -0500

    Starts CORS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Common
 import express from 'express'
+import cors, { CorsOptions } from 'cors'
 import bodyParser from 'body-parser'
 
 // Routes
@@ -7,8 +8,25 @@ import routes from 'routes/v1'
 
 const app = express()
 
+// Parse JSON request body
 app.use(bodyParser.json())
+
+// Parse urlencoded request body
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// Enable CORS
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (origin?.includes('brianmatthewsmith')) {
+      return callback(null, true)
+    }
+    return callback(new Error('Not allowed by CORS'))
+  },
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
+
+// v1 API routes
 app.use('/v1', routes)
 
 app.get('/', (_, res) => {
